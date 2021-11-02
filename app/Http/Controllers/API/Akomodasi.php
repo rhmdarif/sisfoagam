aa185dc4daff447e7495de8644194f36d958f852
ahmadfajri <ahmadfajri.fajri6@gmail.com>
2021-11-02 10:00:41 +0700

    tambah storage

--- app/Http/Controllers/API/Akomodasi.php
+++ app/Http/Controllers/API/Akomodasi.php
@@ -5,16 +5,24 @@ namespace App\Http\Controllers\API;
 use App\Helper\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Models\Akomodasi as ModelsAkomodasi;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class Akomodasi extends Controller
 {
-    public function get()
+    public function get($slugkategori = null, $slugakomodasi = null)
     {
-        $data = ModelsAkomodasi::with(["kategori", "fasilitas"])->get();
-        $data->makeHidden('kategori_akomodasi_id');
-        return $data;
-
-        return response()->json(ApiResponse::Ok($data, 200, "Ok"));
+        try {
+            if ($slugakomodasi) {
+                $data = ModelsAkomodasi::with(["kategori", "fasilitas"])->where("slug_akomodasi", $slugakomodasi)->get();
+            } else {
+                $data = ModelsAkomodasi::with(["kategori", "fasilitas"])->get();
+            }
+            $data->makeHidden('kategori_akomodasi_id');
+            return $data;
+            return response()->json(ApiResponse::Ok($data, 200, "Ok"));
+        } catch (ModelNotFoundException $e) {
+            return response()->json(ApiResponse::NotFound("Data Tidak Ditemukan"));
+        }
     }
 }
