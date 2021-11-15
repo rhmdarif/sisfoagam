845fbcbcada089faa5d6b31464d15a204863629d
Rahmad Arif <rahmadarif28080@gmail.com>
2021-11-15 10:21:20 +0700

    fix destinasi gallery

--- app/Http/Controllers/Api/GaleryController.php
+++ app/Http/Controllers/Api/GaleryController.php
@@ -16,18 +16,22 @@ class GaleryController extends Controller
             case 'akomodasi':
                 $t_foto_video = "foto_video_akomodasi";
                 $t_kategori = "akomodasi";
+                $f_slug = "slug_akomodasi";
                 break;
             case 'ekonomi-kreatif':
                 $t_foto_video = "foto_video_ekonomi_kreatif";
                 $t_kategori = "ekonomi_kreatif";
+                $f_slug = "slug_ekonomi_kreatif";
                 break;
             case 'fasilitas-umum':
                 $t_foto_video = "foto_video_fasilitas_umum";
                 $t_kategori = "fasilitas_umum";
+                $f_slug = "slug_fasilitas_umum";
                 break;
             case 'destinasi':
                 $t_foto_video = "destinasi_wisata_foto_vidio_wisata";
                 $t_kategori = "destinasi_wisata";
+                $f_slug = "slug_destinasi";
                 break;
 
             default:
@@ -38,7 +42,7 @@ class GaleryController extends Controller
         $data = DB::table($t_foto_video)
                     ->select($t_foto_video.'.kategori', $t_foto_video.'.file')
                     ->join($t_kategori, $t_kategori.".id", $t_foto_video.".".$t_kategori."_id")
-                    ->where($t_kategori.".slug_".$t_kategori, $slug)
+                    ->where($t_kategori.".".$f_slug, $slug)
                     ->get();
         return response()->json(ApiResponse::Ok($data, 200, "Ok"));
     }
