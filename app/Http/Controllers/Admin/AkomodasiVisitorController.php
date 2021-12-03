36928ca00ddd0f579d450b3ce72fab36229cb92c
Rahmad Arif <rahmadarif28080@gmail.com>
2021-12-03 10:28:27 +0700

    akomodasi destinasi pengunjung

---
+++ app/Http/Controllers/Admin/AkomodasiVisitorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AkomodasiVisitorController extends Controller
+{
+    //
+    public function index()
+    {
+        # code...
+    }
+}
